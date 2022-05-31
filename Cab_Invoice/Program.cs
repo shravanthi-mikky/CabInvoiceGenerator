693c3712b0cac20c6015b85cc6b55a0f062db60b
shravanthi-mikky <shravanthi27041996@gmail.com>
2022-05-31 16:57:31 +0530

    [shravanthi] - Display Average, number of rides, Aggregare fare for multiple rides

--- Cab_Invoice/Program.cs
+++ Cab_Invoice/Program.cs
@@ -5,7 +5,7 @@ Console.WriteLine("Hello, Welcome to Cab Invoice Generator!");
 
 while (true)
 {
-    Console.WriteLine("Please choose the option :\n1)Calculate Fare\n2)Aggregate Amount for Multiple Rides");
+    Console.WriteLine("Please choose the option :\n1)Calculate Fare\n2)Aggregate Amount for Multiple Rides\n3)Enhanced Invoice");
     int option = Convert.ToInt16(Console.ReadLine());
     switch (option)
     {
@@ -19,6 +19,18 @@ while (true)
             MultipleRides[] rides = { new MultipleRides(10, 20), new MultipleRides(15, 25) };
             double totalfare = uc2.CalculateFare(rides);
             Console.WriteLine("Aggregate fare Value for day is :" + totalfare);
+            break;
+        case 3:
+            InvoiceGenerator uc3 = new();
+            MultipleRides[] rides1 = { new MultipleRides(10, 20), new MultipleRides(15, 25) };
+            double totalFare = uc3.CalculateFare(rides1);
+            int numOfRides = uc3.count;
+            double avg = totalFare / numOfRides;
+            Console.WriteLine("************** Invoice **************");
+            Console.WriteLine("Total Number of rides : " + numOfRides);
+            Console.WriteLine("Aggregate fare Value for day is :" + totalFare);
+            Console.WriteLine("Average of all the rides :" + avg);
+            Console.WriteLine("-------------------------------------");
             break;
         default:
             Console.WriteLine("Please choose the correct option!");
--- Cab_InvoiceTests/InvoiceGeneratorTests.cs
+++ Cab_InvoiceTests/InvoiceGeneratorTests.cs
@@ -27,5 +27,15 @@ namespace Cab_Invoice.Tests
             double totalfare = uc2.CalculateFare(rides);
             Assert.AreEqual(295, totalfare);
         }
+        [TestMethod()]
+        public void CalculateFareTest2()
+        {
+            InvoiceGenerator uc3 = new();
+            MultipleRides[] rides1 = { new MultipleRides(10, 20), new MultipleRides(15, 25) };
+            double totalFare = uc3.CalculateFare(rides1);
+            int numOfRides = uc3.count;
+            double avg = totalFare / numOfRides;
+            Assert.AreEqual(147.5, avg);
+        }
     }
 }
