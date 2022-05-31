90e2249de0ebcb2c1121c310ce992b95f1105271
shravanthi-mikky <shravanthi27041996@gmail.com>
2022-05-31 12:52:37 +0530

    [shravanthi] - Calculation Fare for Ride

---
+++ Cab_Invoice/InvoiceGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cab_Invoice
+{
+    public class InvoiceGenerator
+    {
+        public double CostPerKm = 10;
+        public int CostPerMinute = 1;
+        public int MinimumFare = 5;
+        public double CalculateFare(double Distance,int Time)
+        {
+            double Fare = (CostPerKm * Distance) + (CostPerMinute * Time);
+            return Math.Max(Fare, MinimumFare);
+        }
+    }
+}
--- Cab_Invoice/Program.cs
+++ Cab_Invoice/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Cab_Invoice;
+
 Console.WriteLine("Hello, Welcome to Cab Invoice Generator!");
+InvoiceGenerator uc1 = new();
+double fare1 = uc1.CalculateFare(10, 30);
+Console.WriteLine("Total Fare for your Journey is :"+fare1);
